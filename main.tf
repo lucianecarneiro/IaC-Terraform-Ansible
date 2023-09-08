6b29fbd4611dd208a8cbc54e9f0ec748bc0f3790
luciane <lucianecarneiro@outlook.com>
2023-09-08 13:29:29 -0300

    nova máquina com python3 e virtualenv

--- main.tf
+++ main.tf
@@ -16,14 +16,8 @@ resource "aws_instance" "app_server" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
     key_name = "iac-alura"
-    # user_data = <<-EOF
-    #             #!/bin/bash
-    #             cd /home/ubuntu
-    #             echo "<h1>Criado pelo Terraform!<h1>" > index.html
-    #             nohup busybox httpd -f -p 8080 &
-    #             EOF
     user_data_replace_on_change = true
     tags = {
-        Name = "Teste aws"
+        Name = "Terraform Ansible Python"
     }
 }
